d2ab52f29c5745c689f246a61f889b9bc665c8ae
Sten Storgaard <sten.storgaard@gmail.com>
2017-05-21 23:53:45 +0200

    chapter 8 - Add task completed

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Sten Storgaard on 21/05/17.
+//  Copyright © 2017 gizmodk. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        // Create a task from outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        // Add new task to array in previous viewController
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+    }
+
+
+}
---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Sten Storgaard on 21/05/17.
+//  Copyright © 2017 gizmodk. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
